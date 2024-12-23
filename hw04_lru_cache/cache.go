204c170c4389282b3290e87cb9609114876d4184
Ullubiy Abdurakhmanov <u.abdurakhmanov@arenadata.io>
2024-12-23 21:02:25 +0300

    HW04 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "fmt"
+
 type Key string
 
 type Cache interface {
@@ -9,17 +11,71 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
+type cacheItem struct {
+	key   Key
+	Value interface{}
+}
+
 func NewCache(capacity int) Cache {
-	return &lruCache{
-		capacity: capacity,
-		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+	Cache := lruCache{}
+	Cache.capacity = capacity
+	Cache.queue = NewList()
+	Cache.items = make(map[Key]*ListItem)
+	return &Cache
+}
+
+func (l lruCache) Set(key Key, value interface{}) bool {
+	if _, ok := l.items[key]; ok {
+		newCacheItem := cacheItem{}
+		newCacheItem.Value = value
+		newCacheItem.key = key
+
+		l.items[key].Value = newCacheItem
+		i := l.items[key]
+		l.queue.MoveToFront(i)
+		return true
+	}
+	newCacheItem := cacheItem{}
+	newCacheItem.Value = value
+	newCacheItem.key = key
+
+	i := l.queue.PushFront(newCacheItem)
+	l.items[key] = i
+	if l.queue.Len() > l.capacity {
+		var j = l.queue.Back().Value
+		i := j.(cacheItem)
+		delete(l.items, i.key)
+		l.queue.Remove(l.queue.Back())
+	}
+	return false
+}
+
+func (l lruCache) Get(key Key) (interface{}, bool) {
+	if _, ok := l.items[key]; ok {
+		l.queue.MoveToFront(l.items[key])
+		v := l.items[key].Value
+		switch i := v.(type) {
+		case cacheItem:
+			return i.Value, true
+		default:
+			fmt.Printf("\nUnknown type in func GET %T!\n", v)
+		}
+	} else {
+		return nil, false
+	}
+	return nil, false
+}
+
+func (l lruCache) Clear() {
+	for k, v := range l.items {
+		var j = l.items[k].Value
+		i := j.(cacheItem)
+		delete(l.items, i.key)
+		l.queue.Remove(v)
 	}
 }
