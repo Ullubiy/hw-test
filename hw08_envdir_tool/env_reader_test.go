1be2de80cf5b7da1c17979833da629c2b2df0f24
Ullubiy Abdurakhmanov <u.abdurakhmanov@arenadata.io>
2025-01-16 20:01:54 +0300

    HW08 completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,125 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/dchest/safefile"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("чтение файла из директории", func(t *testing.T) {
+		err := os.Mkdir("/tmp/testdir", 0755)
+		defer os.RemoveAll("/tmp/testdir")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		tmpfile, err := safefile.Create("/tmp/testdir/S.txt", 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer tmpfile.Close()
+
+		content := []byte("temporary")
+		if _, err := tmpfile.Write(content); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := tmpfile.Commit(); err != nil {
+			log.Fatal(err)
+		}
+
+		var actual Environment
+		actual, err = ReadDir("/tmp/testdir")
+		if err != nil {
+			fmt.Println(err)
+		}
+		for k, v := range actual {
+			actual[k] = v
+		}
+		var expected Environment
+		expected = map[string]string{
+			"S.txt": "temporary",
+		}
+		result := reflect.DeepEqual(expected, actual)
+		assert.True(t, result)
+	})
+
+	t.Run("чтение невалидного файла", func(t *testing.T) {
+		err := os.Mkdir("/tmp/testdir", 0755)
+		defer os.RemoveAll("/tmp/testdir")
+		if err != nil {
+			fmt.Println(err)
+		}
+		tmpfile, err := safefile.Create("/tmp/testdir/S=q.txt", 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer tmpfile.Close()
+
+		content := []byte("temporary")
+		if _, err := tmpfile.Write(content); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := tmpfile.Commit(); err != nil {
+			log.Fatal(err)
+		}
+
+		var actual Environment
+		actual, err = ReadDir("/tmp/testdir")
+		if err != nil {
+			fmt.Println(err)
+		}
+		for k, v := range actual {
+			actual[k] = v
+		}
+		var expected Environment
+		expected = map[string]string{}
+
+		result := reflect.DeepEqual(expected, actual)
+		assert.True(t, result)
+	})
+
+	t.Run("чтение файла состоящего из нескольких строк", func(t *testing.T) {
+		err := os.Mkdir("/tmp/testdir", 0755)
+		defer os.RemoveAll("/tmp/testdir")
+		if err != nil {
+			fmt.Println(err)
+		}
+		tmpfile, err := safefile.Create("/tmp/testdir/file.txt", 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer tmpfile.Close()
+
+		content1 := []byte("the first line\nthe second line\nthe third line")
+		if _, err := tmpfile.Write(content1); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := tmpfile.Commit(); err != nil {
+			log.Fatal(err)
+		}
+
+		var actual Environment
+		actual, err = ReadDir("/tmp/testdir")
+		if err != nil {
+			fmt.Println(err)
+		}
+		for k, v := range actual {
+			actual[k] = v
+		}
+		var expected Environment
+		expected = map[string]string{
+			"file.txt": "the first line",
+		}
+		result := reflect.DeepEqual(expected, actual)
+		assert.True(t, result)
+	})
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("Успешное выполнение команды", func(t *testing.T) {
+		env := map[string]string{
+			"STR1": "foo",
+			"STR2": "bar",
+		}
+		cmd := []string{"bash", "-c", "echo $STR1$STR2"}
+		code := RunCmd(cmd, env)
+
+		fmt.Println(cmd)
+		assert.Equal(t, 0, code)
+	})
+
+	t.Run("Ошибка при выполнении невалидной команды", func(t *testing.T) {
+		env := map[string]string{}
+		cmd := []string{"bash", "-c", "ls /xxx"}
+		code := RunCmd(cmd, env)
+
+		fmt.Println(cmd)
+		assert.Equal(t, 1, code)
+	})
 }
